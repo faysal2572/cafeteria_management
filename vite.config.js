5b65b50319ab3b2272b9b2e91b9a9fcb7ec3463a
faysal2572 <faysalabir125@gmail.com>
2024-12-30 03:36:20 +0600

    edit for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
-import laravel from '@vitejs/plugin-laravel';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +9,12 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
+        react(),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
 });
