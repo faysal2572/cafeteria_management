4f1d0821da490c52784bdd8b87428fd660fa16f1
faysal2572 <faysalabir125@gmail.com>
2024-12-30 11:33:08 +0600

    basic edit

--- database/migrations/2024_12_12_214934_add_userid_field_to_carts.php
+++ database/migrations/2024_12_12_214934_add_userid_field_to_carts.php
@@ -11,9 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('carts', function (Blueprint $table) {
-            $table->string('userid')->nullable()->after('price');
-        });
+        // Migration removed as user_id column already exists in create_carts_table migration
     }
 
     /**
@@ -21,8 +19,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('carts', function (Blueprint $table) {
-            $table->dropColumn('userid');
-        });
+        // No action needed
     }
 };
